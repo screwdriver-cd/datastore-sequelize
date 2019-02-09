c82fb9be311cdeaaec5437a54b421a8bda03075f
parthasl <parthasl@yahoo-inc.com>
2019-02-08 18:52:58 -0800

    fix: prevent alter statement firing without any schema change

--- index.js
+++ index.js
@@ -218,7 +218,7 @@ class Squeakquel extends Datastore {
      * @return {Promise}
      */
     setup() {
-        return this.client.sync({ alter: true });
+        return this.client.sync();
     }
 
     /**
