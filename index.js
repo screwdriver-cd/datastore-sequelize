22eb0c944c43a0e1136ab9785c1117f97ec27ba1
parthasl <parthasl@yahoo-inc.com>
2019-02-08 18:59:20 -0800

    fix: revert alter statement

--- index.js
+++ index.js
@@ -218,7 +218,7 @@ class Squeakquel extends Datastore {
      * @return {Promise}
      */
     setup() {
-        return this.client.sync();
+        return this.client.sync({ alter: true });
     }
 
     /**
